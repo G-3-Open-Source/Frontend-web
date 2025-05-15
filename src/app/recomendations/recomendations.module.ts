65364708c512d012d0b0202b3478eb83f1b2e13b
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-05-14 23:00:58 -0500

    initialization uploaded

---
+++ src/app/recomendations/recomendations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecomendationsRoutingModule } from './recomendations-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecomendationsRoutingModule
+  ]
+})
+export class RecomendationsModule { }
