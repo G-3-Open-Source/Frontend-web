db6fbee68cc4e02108ea823214e79546b9f48b96
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-05-16 01:13:02 -0500

    routes modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
-import {LayoutComponent} from './public/components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
+    redirectTo: 'meal-plan',
+    pathMatch: 'full',
+  },
       {path: 'meal-plan',
         loadChildren: () =>
           import('./meal-plan/meal-plan.module').then(m => m.MealPlanModule)
       },
-    ]
-  }
+
 ];
