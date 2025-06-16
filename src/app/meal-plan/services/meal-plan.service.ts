c33c6b7e4a1ff0b2612fb96cdcb73062384b3e50
jlinares <jorgelinaresrrs301@gmail.com>
2025-06-16 17:21:16 -0500

    shared modified

--- src/app/meal-plan/services/meal-plan.service.ts
+++ src/app/meal-plan/services/meal-plan.service.ts
@@ -4,15 +4,18 @@ import {forkJoin, map, Observable, switchMap} from 'rxjs';
 import {MealPlan, MealPlanTags} from '../model/meal-plan.entity';
 import {MealPlanResponse} from './meal-plan.response';
 import { MealPlanAssembler} from './meal-plan.assembler';
+import { BaseService} from '../../shared/services/base.service';
 
 import {environment as env} from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class MealPlanService {
-  constructor(private http: HttpClient) {
+export class MealPlanService extends BaseService<MealPlan> {
+  constructor(protected override http: HttpClient) {
+    super(http, '/meal_plans');
   }
 
+  //GET
   getAllMealPlans(): Observable<MealPlan[]> {
     return this.http.get<MealPlanResponse[]>(`${env.apiUrl}/meal_plans`).pipe(
       map(data => MealPlanAssembler.toEntityFromResponseArray(data)),
@@ -27,23 +30,34 @@ export class MealPlanService {
   getTags(): Observable<any> {
     return this.http.get<MealPlanTags>(`${env.apiUrl}/meal_plan_tags`);
   }
-updateMealPlan(mealPlanid: string, MealPlan: MealPlan): Observable<any> {
-    return this.http.put<MealPlanResponse>(`${env.apiUrl}/meal_plans/${mealPlanid}`, MealPlan).pipe(
+
+  getDetailsMealPlanbyId(id: string): Observable<MealPlan> {
+    return this.http.get<MealPlanResponse>(`${env.apiUrl}meal_plans/${id}`).pipe(
       map(data => MealPlanAssembler.toEntityFromResponse(data))
     );
   }
+
+
+  //POST
   createMealPlan(mealPlan: MealPlan): Observable<any> {
     return this.http.post<MealPlanResponse>(`${env.apiUrl}/meal_plans`, mealPlan).pipe(
       map(data => MealPlanAssembler.toEntityFromResponse(data))
     );
   }
+
+  //PUT
   saveMealPlan(mealPlanId: string, mealPlan: MealPlan): Observable<any> {
     return this.http.put<MealPlanResponse>(`${env.apiUrl}/meal_plans/${mealPlanId}`, mealPlan).pipe(
       map(data => MealPlanAssembler.toEntityFromResponse(data))
     );
   }
+  updateMealPlan(mealPlanid: string, MealPlan: MealPlan): Observable<any> {
+    return this.http.put<MealPlanResponse>(`${env.apiUrl}/meal_plans/${mealPlanid}`, MealPlan).pipe(
+      map(data => MealPlanAssembler.toEntityFromResponse(data))
+    );
+  }
 
-
+  //DELETE
   deleteMealPlan(id: string): Observable<any> {
     return this.http.delete(`${env.apiUrl}/meal_plans/${id}`);
   }
@@ -51,10 +65,5 @@ updateMealPlan(mealPlanid: string, MealPlan: MealPlan): Observable<any> {
     return this.http.delete(`${env.apiUrl}meal_plan_recipes/${id}`);
   }
 
-  getDetailsMealPlanbyId(id: string): Observable<MealPlan> {
-    return this.http.get<MealPlanResponse>(`${env.apiUrl}meal_plans/${id}`).pipe(
-      map(data => MealPlanAssembler.toEntityFromResponse(data))
-    );
-  }
 
 }
