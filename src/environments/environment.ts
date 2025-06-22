3624f8b8115e0e595efb00139fcaa711d9f44d77
jlinares <jorgelinaresrrs301@gmail.com>
2025-06-22 18:31:52 -0500

    env produc changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 // Server Base Path for Fake REST API
-  serverBasePath: 'http://localhost:3000/api/v1',
+  serverBasePath: 'https://web-service-production-54ff.up.railway.app',
 // Server Base Path for Spring Boot REST API
 //serverBasePath: 'http://localhost:8090/api/v1',
   ingredientsEndpointPath: '/ingredients',
